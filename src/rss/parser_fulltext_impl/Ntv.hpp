6deb2842047fc0da5682107839c7e9313079d101
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-10-04 23:49:34 +0200

    (most) NTV texts can be queried

--- src/rss/parser_fulltext_impl/Ntv.hpp
+++ src/rss/parser_fulltext_impl/Ntv.hpp
@@ -19,17 +19,31 @@ namespace RSS
             return std::nullopt;
         }
         tinyxml2::XMLDocument doc;
-        const auto error = doc.Parse(tinyxml2::strip_javascript(article.text).c_str());
+
+
+        // strip useless tags / tags which would crash tinyxml2
+        static const std::vector<std::string> tags_to_strip{
+            "script", "link", "style", "meta", "base", "img", "source", "input"
+        };
+
+        std::string html_stripped = article.text;
+        for(const auto& tag : tags_to_strip) html_stripped = tinyxml2::strip_tag(html_stripped, tag);
+
+        const auto error = doc.Parse(html_stripped.c_str());
         if(error != 0)
         {
-            SPDLOG_WARN("Error while parsing {}: {}\nGet-Response:\n{}", url, error, article.text);
+            SPDLOG_WARN("Error while parsing {}:\nError: {}\nContext: {}", url,doc.ErrorStr(), html_stripped);
+
+            std::ofstream f_out;
+            f_out.open("full.txt");
+            f_out << article.text;
+            f_out.close();
             return std::nullopt;
         }
-        SPDLOG_DEBUG("entering get_first");
+
         // NTV stores information is several <p> tags in <div class="article__text">
-        // TODO: doesnt work
         const auto div_node = tinyxml2::get_first_by_attribute(doc.RootElement(), "class", "article__text");
-        SPDLOG_DEBUG("finished get_first");
+
         if(!div_node.has_value())
         {
             SPDLOG_WARN("Article {} doesn't match expected Pattern for N-TV.", url);
@@ -39,10 +53,10 @@ namespace RSS
         //grab add append all <p> tags
         std::string fulltext;
         const auto ps = tinyxml2::get_all_by_tag(div_node.value(), "p");
-        SPDLOG_INFO("Found {} <p> tags", ps.size());
-        for(const auto& p : ps)
+        SPDLOG_DEBUG("Found {} <p> tags", ps.size());
+        for(const auto* p : ps)
         {
-            fulltext += std::string(p->GetText()) + "\n";
+            if(p->GetText()) fulltext += std::string(p->GetText()) + "\n";
         }
 
         return fulltext;
