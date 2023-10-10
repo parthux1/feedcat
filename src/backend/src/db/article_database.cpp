038789360d191661deba0d5cb28d312935dc4733
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-10-10 16:48:12 +0200

    add sqlit3 dependency

---
+++ src/backend/src/db/article_database.cpp
@@ -0,0 +1,5 @@
+//
+// Created by parthu on 10/5/23.
+//
+
+#include "article_database.hpp"
---
+++ src/backend/src/db/article_database.hpp
@@ -0,0 +1,14 @@
+//
+// Created by parthu on 10/5/23.
+//
+
+#ifndef FEEDCAT_ARTICLE_DATABASE_HPP
+#define FEEDCAT_ARTICLE_DATABASE_HPP
+
+
+class ArticleDatabase {
+
+};
+
+
+#endif //FEEDCAT_ARTICLE_DATABASE_HPP
