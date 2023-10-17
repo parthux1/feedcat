4be873f9e2d52300e42d0e45b9cafc186bf9d925
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-10-18 00:55:59 +0200

    iml. via meyers singleton

--- src/backend/db/database_sqlite.cpp
+++ src/backend/db/database_sqlite.cpp
@@ -1,29 +1,10 @@
 //
 // Created by parthu on 10/5/23.
 //
-#include "article_database.hpp"
+#include "database_sqlite.hpp"
 
-std::mutex ArticleDatabase::myMutex = std::mutex{};
-auto ArticleDatabase::instance = std::atomic(make_shared<ArticleDatabase>(new ArticleDatabase()));
-
-ArticleDatabase* ArticleDatabase::getInstance(){
-    ArticleDatabase* inst = instance.load(std::memory_order_acquire);
-    if ( !inst ){
-        std::lock_guard<std::mutex> myLock(myMutex);
-        inst = instance.load(std::memory_order_relaxed);
-        if( !inst ){
-            inst = new ArticleDatabase();
-            instance.store(inst,std::memory_order_release);
-        }
-    }
-    return inst;
-}
-
-bool ArticleDatabase::date_valid(const std::string& date)
-{
-    // only accept sqlite3 conform time stamps (str, but special)
-    // ISO8601 "YYYY-MM-DD HH:MM:SS.SSS"
-
-    // TODO: impl
-    return true;
+std::shared_ptr<DatabaseSqlLite> DatabaseSqlLite::getInstance(){
+    // meyers singleton
+    static auto instance = std::atomic{std::shared_ptr<DatabaseSqlLite>(new DatabaseSqlLite())};
+    return instance;
 }
--- src/backend/db/database_sqlite.hpp
+++ src/backend/db/database_sqlite.hpp
@@ -1,27 +1,26 @@
 /*
  * Created by parthu on 10/5/23.
- * DB Implementation as Singleton
+ * SqlLite Singleton Implementation
 */
 #pragma once
 
 #include <atomic>
 #include <mutex>
 #include <memory>
+#include <db/database_interface.hpp>
 
-class ArticleDatabase
+class DatabaseSqlLite : public DatabaseInterface
 {
 public:
-    static std::shared_ptr<ArticleDatabase> getInstance();
+    static std::shared_ptr<DatabaseSqlLite> getInstance();
 
-    static bool date_valid(const std::string& date);
+    // Singleton behavior
+    DatabaseSqlLite(const DatabaseSqlLite&) = delete;
+    DatabaseSqlLite& operator=(const DatabaseSqlLite&) = delete;
 
+    // leaked for static shared_ptr instance in getInstance()
+    ~DatabaseSqlLite() = default;
 private:
-    ArticleDatabase()= default;
-    ~ArticleDatabase()= default;
-    ArticleDatabase(const ArticleDatabase&)= delete;
-    ArticleDatabase& operator=(const ArticleDatabase&)= delete;
-
-    static std::atomic<std::shared_ptr<ArticleDatabase>> instance;
-    static std::mutex myMutex;
+    DatabaseSqlLite() = default;
 };
 
