5b6e1a71f9bca9ca74369b54265914444c16ace9
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-10-18 15:33:47 +0200

    ntv download and cache example

--- src/console/main.cpp
+++ src/console/main.cpp
@@ -1,6 +1,6 @@
 #include <cpr/cpr.h>
 #include <spdlog/spdlog.h>
-#include <rss/xml_parser.hpp>
+#include <rss/parser_rss.hpp>
 #include <rss/parser_fulltext_impl/Ntv.hpp>
 #include <db/database_sqlite.hpp>
 
@@ -10,32 +10,36 @@ int main() {
     //query RSS feed
     // ntv: "https://www.n-tv.de/politik/rss"
     // heise: "https://www.heise.de/rss/heise-Rubrik-IT.rdf"
-    cpr::Response r = cpr::Get(cpr::Url{"https://www.n-tv.de/politik/rss"});
 
-    auto res = RSS::XML::parse(r.text);
+    std::list<std::string> urls_ntv = {"https://www.n-tv.de/wirtschaft/rss", "https://www.n-tv.de/politik/rss"};
 
-    SPDLOG_INFO("Got {} articles. Querying fulltext", res.size());
-    RSS::ParserFulltext<RSS::ProviderNTV> p_ntv;
-
-    for(auto& a : res)
+    for(const auto& url : urls_ntv)
     {
-        if(!RSS::ParserFulltext<RSS::ProviderNTV>::get_fulltext(a))
-        {
-            SPDLOG_CRITICAL("Download of article {} failed.", a.title);
-        }
-    }
-    std::size_t count = 0;
-    SPDLOG_INFO("Saving to database.");
-    auto db = DatabaseSqlLite::get_instance();
-    for(const auto& a : res)
-    {
-        if(!db->article_exists(a))
-        {
-            db->store_article(a);
-            count++;
-        }
-    }
-    SPDLOG_INFO("Saved {} new articles to database.", count);
+        SPDLOG_INFO("Refreshing {}", url);
 
+        cpr::Response r = cpr::Get(cpr::Url{url});
+        auto res = RSS::XML::parse(r.text);
+
+        auto db = DatabaseSqlLite::get_instance();
+        RSS::ProviderNTV p_ntv{};
+        std::size_t count = 0;
+
+        for(auto& a : res)
+        {
+            if(!db->article_exists(a))
+            {
+                if(p_ntv.get_fulltext(a))
+                {
+                    db->store_article(a);
+                    count++;
+                }
+                else
+                {
+                    SPDLOG_WARN("failed to fetch fulltext of URL: {}", a.url);
+                }
+            }
+        }
+        SPDLOG_INFO("{}/{} articles were new and got saved to local database.", count, res.size());
+    }
     return 0;
 }
