b7f81477099c4e17dec90e356e0f04381f617427
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-11-18 03:59:00 +0100

    missing rss-namespace

--- src/backend/rss/FullTextParserStrategy.hpp
+++ src/backend/rss/FullTextParserStrategy.hpp
@@ -3,4 +3,18 @@
 //
 #pragma once
 
+#include <string>
+#include <optional>
 
+namespace RSS
+{
+    class FullTextParserStrategy
+    {
+    public:
+        /*!
+         * \brief Returns the fulltext of the article at the given url.
+         * \param article_url
+         */
+        virtual std::optional<std::string> get_fulltext(const std::string &article_url) const = 0;
+    };
+}
