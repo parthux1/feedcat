5f6e62cc49af44596fdf49c6d811a5f53dff77dd
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-12-07 22:19:34 +0100

    explicit constructor tests

--- tests/backend/core/tests_ArticlePropertyHash.cpp
+++ tests/backend/core/tests_ArticlePropertyHash.cpp
@@ -7,20 +7,42 @@
 #include <ArticlePropertyHash.hpp>
 #include <properties/ArticleBaseProperty.hpp>
 
+TEST(ArticlePropertyHash, hash_similar_ptr)
+{
+    ArticleBaseProperty dummy_property{"", "","", ""};
+    ASSERT_EQ(ArticlePropertyHash{&dummy_property}, ArticlePropertyHash{&dummy_property});
+}
+
+TEST(ArticlePropertyHash, hash_similar_ref)
+{
+    ArticleBaseProperty dummy_property{"", "","", ""};
+    ASSERT_EQ(ArticlePropertyHash{dummy_property}, ArticlePropertyHash{dummy_property});
+}
+
 /*!
  * \brief Different Constructors have varying implementations finally using typeid. Check if correct values are stored.
  */
-TEST(ArticlePropertyHash, DifferentConstructorComparison)
+TEST(ArticlePropertyHash, hash_similar_between_constructors)
 {
     // Setup
     ArticleBaseProperty dummy_property{"", "","", ""};
 
     const ArticlePropertyHash hash_from_class = ArticlePropertyHash::create<ArticleBaseProperty>();
-    const ArticlePropertyHash hash_from_instance{&dummy_property};
+    const ArticlePropertyHash hash_from_ptr{&dummy_property};
+    const ArticlePropertyHash hash_from_ref{&dummy_property};
 
     const auto std_hash_from_class = std::hash<ArticlePropertyHash>{}(hash_from_class);
-    const auto std_hash_from_instance = std::hash<ArticlePropertyHash>{}(hash_from_instance);
+    const auto std_hash_from_ptr = std::hash<ArticlePropertyHash>{}(hash_from_ptr);
+    const auto std_hash_from_ref = std::hash<ArticlePropertyHash>{}(hash_from_ref);
 
-    ASSERT_EQ(hash_from_class.get(), hash_from_instance.get());
-    ASSERT_EQ(std_hash_from_class, std_hash_from_instance);
+    // check our Hash container
+    ASSERT_EQ(hash_from_ref, hash_from_ptr);
+    ASSERT_EQ(hash_from_class, hash_from_ref);
+
+    // std:: Hash container
+    ASSERT_EQ(std_hash_from_ref, std_hash_from_ptr);
+    ASSERT_EQ(std_hash_from_class, std_hash_from_ptr);
 }
+
+
+
