79a379fd639b0cad681664644c4095214b611794
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-12-12 14:55:47 +0100

    move free template functions in same file as db interface

--- src/backend/core/db/database_interface.hpp
+++ src/backend/core/db/database_interface.hpp
@@ -1,18 +1,63 @@
-//
-// Created by parthu on 10/18/23.
-//
+/*
+ * This file contains functions needed for database-property interaction
+ *
+ * Why not use an actual Strategy Pattern:
+ * + static storage would allow registering consistent names for all properties.
+ * - no static typeCheck for implemented Strategy bc. it accepts ArticlePropertyInterface*
+ *   Could be done via visitor, but this would require a variant including all Property-Implementations
+ *   A visitor can also be implemented via these free template functions
+ */
 #pragma once
 
 #include <string>
 #include <optional>
+#include <concepts>
+#include <ArticlePropertyInterface.hpp>
 
-// must be very basic, Properties define how to store and load themselves
-class DatabaseInterface
+// free template functions
+
+template<typename T>
+requires std::derived_from<T, ArticlePropertyInterface>
+bool store_property(const T* property, DatabaseInterface* db) = delete;
+
+// TODO: additional information for loading required
+template<typename T>
+requires std::derived_from<T, ArticlePropertyInterface>
+std::optional<T> load_property(DatabaseInterface* db) = delete;
+
+template<typename T>
+/*!
+ * \brief Use this concept to check if all needed functions for loose PropertyDbStrategy are implemented
+ * \note Apply this concept after overloading functions. Otherwise the call to decltype() will default to base templates
+ * \example static_assert(PropertyDbStrategy<MyPropertyType>);
+ * \example See src/backend/properties/ArticleBasePropertyDb.hpp
+ */
+concept PropertyDbStrategy = requires()
 {
+    requires std::derived_from<T, ArticlePropertyInterface>; // Redundant
+    requires std::invocable<decltype(load_property<T>), DatabaseInterface*>;
+    requires std::invocable<decltype(store_property<T>), T*, DatabaseInterface*>;
 };
 
-// old DB-Interface directly working on articles
 
+// Simple Database-oriented wrapper around free template functions
+class DatabaseInterface
+{
+    template<PropertyDbStrategy T>
+    bool store_property(const T* property)
+    {
+        store_property(property, this);
+    }
+
+    template<PropertyDbStrategy T>
+    std::optional<T> load_property()
+    {
+        return load_property<T>(this);
+    }
+};
+
+
+// old DB-Interface directly working on articles
 #if false
 class DatabaseInterface
 {
