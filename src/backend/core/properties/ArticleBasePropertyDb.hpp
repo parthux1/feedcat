9ea650d948413b5783cf6b79dd72771f99069c3b
Yann Arend Zankl <yann.a.zankl@gmail.com>
2023-12-19 02:57:14 +0100

    implement serializer

--- src/backend/core/properties/ArticleBasePropertyDb.hpp
+++ src/backend/core/properties/ArticleBasePropertyDb.hpp
@@ -4,16 +4,53 @@
 #pragma once
 
 #include <properties/ArticleBaseProperty.hpp>
-#include <db/database_interface.hpp>
+#include <db/property_serialize_functions.hpp>
 
 template<>
-std::optional<DatabaseID> store_property(const ArticleBaseProperty* property, DatabaseInterface* db)
+std::string table_name<ArticleBaseProperty>()
 {
-    return std::nullopt;
+    return "article_base";
 }
 
 template<>
-std::optional<ArticleBaseProperty> load_property(DatabaseInterface* db, const DatabaseID& id)
+SerializedMapping serialize_mapping<ArticleBaseProperty>()
 {
+    return {
+        {"url", DatabaseFieldType::TEXT},
+        {"description", DatabaseFieldType::TEXT},
+        {"rss_url", DatabaseFieldType::TEXT},
+        {"title", DatabaseFieldType::TEXT},
+    };
+}
+
+template<>
+SerializedValues serialize(const ArticleBaseProperty& obj)
+{
+    return {
+        {"url", obj.url},
+        {"description", obj.description},
+        {"rss_url", obj.rss_url},
+        {"title", obj.title},
+    };
+}
+
+template<>
+std::optional<ArticleBaseProperty> deserialize(const SerializedValues& obj)
+{
+    if (obj.contains("url") && obj.contains("description") && obj.contains("rss_url") && obj.contains("title"))
+    {
+        try
+        {
+            return ArticleBaseProperty{
+                    std::get<std::string>(obj.at("title")),
+                    std::get<std::string>(obj.at("url")),
+                    std::get<std::string>(obj.at("description")),
+                    std::get<std::string>(obj.at("rss_url")),
+            };
+        }catch (std::bad_variant_access& e)
+        {
+            SPDLOG_ERROR("Could not deserialize ArticleBaseProperty: {}", e.what());
+        }
+    }
     return std::nullopt;
 }
