d5f6de47408703aca9587a4deb3895cf8502b6ff
Yann Arend Zankl <yann.a.zankl@gmail.com>
2024-01-03 21:44:41 +0100

    fix linker problem by moving specializations

---
+++ src/backend/core/properties/ArticleBasePropertyDb.cpp
@@ -0,0 +1,54 @@
+//
+// Created by parthu on 1/3/24.
+//
+
+#include "ArticleBasePropertyDb.hpp"
+
+template<>
+std::string table_name<ArticleBaseProperty>()
+{
+    return "article_base";
+}
+
+template<>
+SerializedMapping serialize_mapping<ArticleBaseProperty>()
+{
+    return {
+            {"url", DatabaseFieldType::TEXT},
+            {"description", DatabaseFieldType::TEXT},
+            {"rss_url", DatabaseFieldType::TEXT},
+            {"title", DatabaseFieldType::TEXT},
+    };
+}
+
+template<>
+SerializedValues serialize(const ArticleBaseProperty& obj)
+{
+    return {
+            {"url", obj.url},
+            {"description", obj.description},
+            {"rss_url", obj.rss_url},
+            {"title", obj.title},
+    };
+}
+
+template<>
+std::optional<ArticleBaseProperty> deserialize(const SerializedValues& obj)
+{
+    if (obj.contains("url") && obj.contains("description") && obj.contains("rss_url") && obj.contains("title"))
+    {
+        try
+        {
+            return ArticleBaseProperty{
+                    std::get<std::string>(obj.at("title")),
+                    std::get<std::string>(obj.at("url")),
+                    std::get<std::string>(obj.at("description")),
+                    std::get<std::string>(obj.at("rss_url")),
+            };
+        }catch (std::bad_variant_access& e)
+        {
+            SPDLOG_ERROR("Could not deserialize ArticleBaseProperty: {}", e.what());
+        }
+    }
+    return std::nullopt;
+}
--- src/backend/core/properties/ArticleBasePropertyDb.hpp
+++ src/backend/core/properties/ArticleBasePropertyDb.hpp
@@ -7,50 +7,13 @@
 #include <core/db/property_serialize_functions.hpp>
 
 template<>
-std::string table_name<ArticleBaseProperty>()
-{
-    return "article_base";
-}
+std::string table_name<ArticleBaseProperty>();
 
 template<>
-SerializedMapping serialize_mapping<ArticleBaseProperty>()
-{
-    return {
-        {"url", DatabaseFieldType::TEXT},
-        {"description", DatabaseFieldType::TEXT},
-        {"rss_url", DatabaseFieldType::TEXT},
-        {"title", DatabaseFieldType::TEXT},
-    };
-}
+SerializedMapping serialize_mapping<ArticleBaseProperty>();
 
 template<>
-SerializedValues serialize(const ArticleBaseProperty& obj)
-{
-    return {
-        {"url", obj.url},
-        {"description", obj.description},
-        {"rss_url", obj.rss_url},
-        {"title", obj.title},
-    };
-}
+SerializedValues serialize(const ArticleBaseProperty& obj);
 
 template<>
-std::optional<ArticleBaseProperty> deserialize(const SerializedValues& obj)
-{
-    if (obj.contains("url") && obj.contains("description") && obj.contains("rss_url") && obj.contains("title"))
-    {
-        try
-        {
-            return ArticleBaseProperty{
-                    std::get<std::string>(obj.at("title")),
-                    std::get<std::string>(obj.at("url")),
-                    std::get<std::string>(obj.at("description")),
-                    std::get<std::string>(obj.at("rss_url")),
-            };
-        }catch (std::bad_variant_access& e)
-        {
-            SPDLOG_ERROR("Could not deserialize ArticleBaseProperty: {}", e.what());
-        }
-    }
-    return std::nullopt;
-}
+std::optional<ArticleBaseProperty> deserialize(const SerializedValues& obj);
